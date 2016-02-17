32fff0789f4862c925b4c7933bce99ba4509fc2c
cococohen <cococohen@gmail.com>
2016-02-17 11:16:17 -0800

    working from Brackets

---
+++ js/cbags.js
@@ -0,0 +1,19 @@
+/**
+ * Created by mark on 1/16/16.
+ */
+
+function lengthFromDiameter() {
+    "use strict";
+    var getText = document.getElementById("circleWidth");
+    var disp = document.getElementById("dispArea");
+    disp.innerHTML = "Crochet for " + (getText.value * 3.14).toPrecision(3) + " inches.";
+    getText.value = "";
+}
+
+function diameterFromLength() {
+    "use strict";
+    var getText = document.getElementById("bodyLength");
+    var disp = document.getElementById("dispArea");
+    disp.innerHTML = "Crochet circle for " + (getText.value / 3.14).toPrecision(3) + " inches.";
+    getText.value = "";
+}
